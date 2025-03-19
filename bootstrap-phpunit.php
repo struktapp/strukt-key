344c3e846f4e8534d3aad21d902184f3e9026fc8
pitsolu <pitsolu@gmail.com>
2025-03-20 00:53:13 +0300

    fixes

--- bootstrap-phpunit.php
+++ bootstrap-phpunit.php
@@ -7,9 +7,9 @@ $loader->add("Strukt",__DIR__."/src/");
 
 if(!Strukt\Fs::isPath("fixture/pitsolu")){
             
-    exec("php console cry:keys pitsolu");
-    exec("php console cry:keys pitsolu_next --pass p@55w0rd");
-    exec("php console cert:selfsign pitsolu cacert.pem");
+    exec("php kcli cry:keys pitsolu");
+    exec("php kcli cry:keys pitsolu_next --pass p@55w0rd");
+    exec("php kcli cert:selfsign pitsolu cacert.pem");
     exec("mv pitsolu* ./fixture");
     exec("mv cacert.pem ./fixture");
 }
